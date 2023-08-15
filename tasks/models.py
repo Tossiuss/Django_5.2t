c489437f31c4af19be816ca86ef5c94025ed225b
Tossiuss <antonio010602@gmail.com>
2023-08-15 16:15:42 +0600

    zadanie_1

---
+++ tasks/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user} on {self.task}"
+
